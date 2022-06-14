ee494c17e93307f60deafc4dc83c2e451f984faa
unu <unu@LABKOM-19>
2022-06-13 19:14:56 -0700

    Sample insert data

---
+++ WindowsApp3/DBConnection.vb
@@ -0,0 +1,15 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module DBConnection
+
+    Public CONN As MySqlConnection = New MySqlConnection()
+
+    Sub OpenConnection()
+        CONN.ConnectionString = "server=localhost;user id=root;password=password;database=test"
+        CONN.Open()
+    End Sub
+
+    Sub CloseConnection()
+        CONN.Close()
+    End Sub
+End Module
--- WindowsApp3/Form1.vb
+++ WindowsApp3/Form1.vb
@@ -1,5 +1,17 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub btn_simpan_Click(sender As Object, e As EventArgs) Handles btn_simpan.Click
+        DBConnection.OpenConnection()
+
+        Dim command = New MySqlCommand("INSERT INTO users values ('" & tb_username.Text & "', '" & tb_password.Text & "', '" & tb_fullname.Text & "')", DBConnection.CONN)
+        command.ExecuteNonQuery()
+
+
+        DBConnection.CloseConnection()
     End Sub
 End Class
