e15bf1a79a35e0165cd59632d20408feef86bd77
unu <unu@LABKOM-19>
2022-06-13 18:57:49 -0700

    Form UI

--- WindowsApp3/Form1.Designer.vb
+++ WindowsApp3/Form1.Designer.vb
@@ -22,17 +22,96 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.tb_fullname = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tb_username = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tb_password = New System.Windows.Forms.TextBox()
+        Me.btn_simpan = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'tb_fullname
+        '
+        Me.tb_fullname.Location = New System.Drawing.Point(70, 9)
+        Me.tb_fullname.Name = "tb_fullname"
+        Me.tb_fullname.Size = New System.Drawing.Size(202, 20)
+        Me.tb_fullname.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Full name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Username"
+        '
+        'tb_username
+        '
+        Me.tb_username.Location = New System.Drawing.Point(70, 35)
+        Me.tb_username.Name = "tb_username"
+        Me.tb_username.Size = New System.Drawing.Size(202, 20)
+        Me.tb_username.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 65)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Password"
+        '
+        'tb_password
+        '
+        Me.tb_password.Location = New System.Drawing.Point(70, 61)
+        Me.tb_password.Name = "tb_password"
+        Me.tb_password.Size = New System.Drawing.Size(202, 20)
+        Me.tb_password.TabIndex = 4
+        '
+        'btn_simpan
+        '
+        Me.btn_simpan.Location = New System.Drawing.Point(116, 98)
+        Me.btn_simpan.Name = "btn_simpan"
+        Me.btn_simpan.Size = New System.Drawing.Size(75, 23)
+        Me.btn_simpan.TabIndex = 6
+        Me.btn_simpan.Text = "Simpan"
+        Me.btn_simpan.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(306, 140)
+        Me.Controls.Add(Me.btn_simpan)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.tb_password)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.tb_username)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tb_fullname)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents tb_fullname As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents tb_username As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents tb_password As TextBox
+    Friend WithEvents btn_simpan As Button
 End Class
